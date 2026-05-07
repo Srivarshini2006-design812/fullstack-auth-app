f8cf130ed08452a5d50e425451cb79a3c70ba5ed
Srivarshini <srivarshininov2006@gmail.com>
2026-05-07 07:46:28 +0000

    fix: persistent login cloud update

--- fastapi-db/app/db.py
+++ fastapi-db/app/db.py
@@ -1,13 +1,5 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_conn():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
